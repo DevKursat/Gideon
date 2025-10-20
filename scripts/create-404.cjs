a902ba46d3947497f74c54e6d7d29cab5304941b
kursatdev2 <ykursat055@gmail.com>
2025-10-20 19:26:00 +0000

    chore: add noscript meta refresh to 404 fallback

--- scripts/create-404.cjs
+++ scripts/create-404.cjs
@@ -13,7 +13,7 @@ if (!fs.existsSync(indexFile)) {
 const htmlContent = fs.readFileSync(indexFile, 'utf8');
 
 // Inject a small redirect snippet to forward SPA routes back to index with a redirect param
-const redirectSnippet = `\n<script>\n(function(){\n  try {\n    const pathname = location.pathname;\n    const segments = pathname.split('/').filter(Boolean);\n    const base = segments.length ? '/' + segments[0] + '/' : '/';\n    // If this is a fallback 404 route, redirect to base with the original path as a query param\n    if (location.search.indexOf('redirect=') === -1 && pathname !== base) {\n      location.replace(base + '?redirect=' + encodeURIComponent(location.pathname + location.search + location.hash));\n    }\n  } catch(e) {}\n})();\n</script>\n`;
+const redirectSnippet = `\n<script>\n(function(){\n  try {\n    const pathname = location.pathname;\n    const segments = pathname.split('/').filter(Boolean);\n    const base = segments.length ? '/' + segments[0] + '/' : '/';\n    // If this is a fallback 404 route, redirect to base with the original path as a query param\n    if (location.search.indexOf('redirect=') === -1 && pathname !== base) {\n      location.replace(base + '?redirect=' + encodeURIComponent(location.pathname + location.search + location.hash));\n    }\n  } catch(e) {}\n})();\n</script>\n<noscript>\n  <meta http-equiv="refresh" content="0; url=/Gideon/">\n</noscript>\n`;
 
 const outHtml = htmlContent.replace('</head>', `${redirectSnippet}</head>`);
 fs.writeFileSync(outFile, outHtml, 'utf8');
