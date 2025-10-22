2ddc9d971cb2c554fd7bc8da89b071c33a063cac
kursatdev2 <ykursat055@gmail.com>
2025-10-22 02:22:36 +0000

    chore(docs): update production build for GH Pages (main/docs)

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,22 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Gideon/',
-  plugins: [react()],
-  build: {
-    outDir: 'docs'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command }) => {
+  const base = command === 'build' ? '/Gideon/' : '/';
+  return {
+    base: base,
+    plugins: [react()],
+    build: {
+      outDir: 'docs'
     },
-  },
-  server: {
-    port: 3000,
-    host: true
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      port: 3000,
+      host: true
+    }
   }
-})
+});
